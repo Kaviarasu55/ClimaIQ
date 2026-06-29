facf9fe9029aa7fcd37dd818a8297d20cd98b208
Kaviarasu E <kaviarasu302006@gmail.com>
2026-06-29 19:19:54 +0530

    update BASE_URL for production

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ─── Config ───────────────────────────────────────────────
-const BASE_URL = "http://127.0.0.1:5000";
+const BASE_URL = "https://climaiq.onrender.com";
 
 // ─── Page Switching ───────────────────────────────────────
 const navItems = document.querySelectorAll(".nav-item");
